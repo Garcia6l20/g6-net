556a8adcd39bc918ae7221184cf9a6041c6a0ea8
Sylvain Garcia <garcia.6l20@gmail.com>
2021-03-03 23:24:35 +0100

    add examples
    - tests are now real tests

---
+++ examples/udp-example.cpp
@@ -0,0 +1,40 @@
+#include <unifex/sync_wait.hpp>
+#include <unifex/task.hpp>
+#include <unifex/when_all.hpp>
+
+#include <g6/io/context.hpp>
+#include <g6/net/ip_endpoint.hpp>
+#include <g6/net/async_socket.hpp>
+
+#include <iostream>
+
+using namespace g6;
+
+int main() {
+    io::context ctx{};
+    inplace_stop_source stop_source{};
+
+    sync_wait(when_all(
+        [&]() -> task<void> {
+            scope_guard _ = [&]() noexcept {
+                stop_source.request_stop();
+            };
+            auto sock = net::open_socket(ctx.get_scheduler(), AF_INET, SOCK_DGRAM);
+            std::array<std::byte, 64> buffer{};
+            sock.bind(*net::ip_endpoint::from_string("127.0.0.1:4242"));
+            auto [bytes_received, from] = co_await g6::net::async_recv_from(sock, as_writable_bytes(span{buffer}));
+            std::cout << "received " << bytes_received << " bytes from '" << from.to_string() << "': "
+                      << std::string_view{reinterpret_cast<char*>(buffer.data()), bytes_received} << '\n';
+            co_return;
+        }(),
+        [&]() -> task<void> {
+            auto sock = net::open_socket(ctx.get_scheduler(), AF_INET, SOCK_DGRAM);
+            const char buffer[] = {"hello world !!!"};
+            auto bytes_sent = co_await g6::net::async_send_to(sock, as_bytes(span{buffer}), *g6::net::ip_endpoint::from_string("127.0.0.1:4242"));
+            co_return;
+        }(),
+        [&]() -> task<void> {
+            ctx.run(stop_source.get_token());
+            co_return;
+        }()));
+}
--- tests/tcp-test.cpp
+++ tests/tcp-test.cpp
@@ -1,42 +1,46 @@
+#include <catch2/catch.hpp>
+
 #include <unifex/sync_wait.hpp>
 #include <unifex/task.hpp>
+#include <unifex/transform.hpp>
 #include <unifex/when_all.hpp>
 
 #include <g6/io/context.hpp>
-#include <g6/net/ip_endpoint.hpp>
 #include <g6/net/async_socket.hpp>
-
-#include <fmt/format.h>
+#include <g6/net/ip_endpoint.hpp>
 
 using namespace g6;
 using namespace std::chrono_literals;
 
-int main() {
+TEST_CASE("tcp tx/rx test", "[g6::net::udp]") {
     io::context ctx{};
     inplace_stop_source stop_source{};
 
     sync_wait(when_all(
-        [&]() -> task<void> {
-            scope_guard _ = [&]() noexcept {
-                stop_source.request_stop();
-            };
-            auto sock = net::open_socket(ctx.get_scheduler(), AF_INET, SOCK_STREAM);
-            sock.bind(*net::ip_endpoint::from_string("127.0.0.1:4242"));
-            sock.listen();
-            auto [client, client_address] = co_await net::async_accept(sock);
-            char buffer[1024]{};
-            auto byte_count = co_await net::async_recv(client, as_writable_bytes(span{buffer}));
-            fmt::print("received: {} bytes from {}: {}\n", byte_count, client_address.to_string(), std::string_view{buffer, byte_count});
-            co_return;
-        }(),
-        [&]() -> task<void> {
-          auto sock = net::open_socket(ctx.get_scheduler(), AF_INET, SOCK_STREAM);
-          co_await net::async_connect(sock, *net::ip_endpoint::from_string("127.0.0.1:4242"));
-          const char buffer[] = {"hello world !!!"};
-          co_await net::async_send(sock, as_bytes(span{buffer}));
-        }(),
-        [&]() -> task<void> {
-            ctx.run(stop_source.get_token());
-            co_return;
-        }()));
+                  [&]() -> task<size_t> {
+                      scope_guard _ = [&]() noexcept {
+                          stop_source.request_stop();
+                      };
+                      auto sock = net::open_socket(ctx.get_scheduler(), AF_INET, SOCK_STREAM);
+                      sock.bind(*net::ip_endpoint::from_string("127.0.0.1:4242"));
+                      sock.listen();
+                      auto [client, client_address] = co_await net::async_accept(sock);
+                      char buffer[1024]{};
+                      auto byte_count = co_await net::async_recv(client, as_writable_bytes(span{buffer}));
+                      co_return byte_count;
+                  }(),
+                  [&]() -> task<size_t> {
+                      auto sock = net::open_socket(ctx.get_scheduler(), AF_INET, SOCK_STREAM);
+                      co_await net::async_connect(sock, *net::ip_endpoint::from_string("127.0.0.1:4242"));
+                      const char buffer[] = {"hello world !!!"};
+                      auto sent = co_await net::async_send(sock, as_bytes(span{buffer}));
+                      co_return sent;
+                  }(),
+                  [&]() -> task<void> {
+                      ctx.run(stop_source.get_token());
+                      co_return;
+                  }()) |
+              transform([](auto &&received, auto &&sent, ...) {
+                  REQUIRE(sent == received);
+              }));
 }

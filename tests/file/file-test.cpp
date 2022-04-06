043281c60ed34fafd3c6dff30d21a3c5f03bc27b
Sylvain Garcia <garcia.6l20@gmail.com>
2022-04-06 09:29:27 +0200

    all tests passing on win32

--- tests/file/file-test.cpp
+++ tests/file/file-test.cpp
@@ -14,14 +14,16 @@ TEST_CASE("file test", "[g6::io::files]") {
     io::context ctx{};
     std::stop_source stop_source{};
 
-    spawner{[&]() -> task<void> {
-                scope_guard _ = [&]() noexcept { stop_source.request_stop(); };
-                auto ro_file = open_file(ctx, __FILE__, open_file_mode::read | open_file_mode::existing);
-                std::array<char, 1024> data{};
-                size_t bytes = co_await async_read_some_at(
-                    ro_file, std::as_writable_bytes(std::span{data.data(), data.size()}), 0);
-                fmt::print("read: {}\n", std::string_view{data.data(), bytes});
-            }(),
-            async_exec(ctx)}
-        .sync_wait();
+    auto [read_bytes, _] = spawner{[&]() -> task<size_t> {
+                                       scope_guard _ = [&]() noexcept { stop_source.request_stop(); };
+                                       auto ro_file =
+                                           open_file(ctx, __FILE__, open_file_mode::read | open_file_mode::existing);
+                                       std::array<char, 1024> data{};
+                                       size_t bytes = co_await async_read_some_at(
+                                           ro_file, std::as_writable_bytes(std::span{data.data(), data.size()}), 0);
+                                       co_return bytes;
+                                   }(),
+                                   async_exec(ctx, stop_source.get_token())}
+                               .sync_wait();
+    REQUIRE(read_bytes != 0);
 }

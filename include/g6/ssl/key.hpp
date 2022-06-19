fd0166c891b15cc883679d0754394e5d22bf874f
Sylvain Garcia <garcia.6l20@gmail.com>
2022-06-19 11:27:10 +0200

    fix ssl::public_key

--- include/g6/ssl/key.hpp
+++ include/g6/ssl/key.hpp
@@ -51,8 +51,12 @@ namespace g6::ssl {
     private:
         detail::mbedtls_pk_context_ptr ctx_ = detail::mbedtls_pk_context_ptr::make();
     };
-    class private_key final : public key<key_type::private_> {};
-    class public_key final : public key<key_type::private_> {};
+    class private_key final : public key<key_type::private_> {
+        using key<key_type::private_>::key;
+    };
+    class public_key final : public key<key_type::public_> {
+        using key<key_type::public_>::key;
+    };
 }// namespace g6::ssl
 
 #include <g6/ssl/impl/pk_impl.hpp>
